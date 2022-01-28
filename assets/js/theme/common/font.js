01ac155f61797470fd6d2f86416c2550ae0c2759
Tony McCreath <tony@websiteadvantage.com.au>
2022-01-28 10:57:41 +1030

    using the pull to put webfonts in the CDN

---
+++ assets/js/theme/common/font.js
@@ -0,0 +1,8 @@
+const WebFont = require('webfontloader');
+
+WebFont.load({
+    custom: {
+        families: ['Roboto:n4', 'Volkhov:n4', 'Montserrat:n5', 'Source Sans Pro'],
+    },
+    classes: false,
+});
