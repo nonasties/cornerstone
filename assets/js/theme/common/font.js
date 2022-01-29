5dd1287130152835b082c30bc52339cf432099c2
Tony McCreath <tony@websiteadvantage.com.au>
2022-01-29 10:43:17 +1030

    inlining css for category page and asyc the main css webfonts.js now lists all possible fonts

--- assets/js/theme/common/font.js
+++ assets/js/theme/common/font.js
@@ -2,7 +2,7 @@ const WebFont = require('webfontloader');
 
 WebFont.load({
     custom: {
-        families: ['Roboto:n4', 'Volkhov:n4', 'Montserrat:n5', 'Source Sans Pro'],
+        families: ['Karla:n4', 'Roboto:n4', 'Volkhov:n4', 'Montserrat:n4,n5', 'Open+Sans:n4,n7', 'Source Sans Pro:n4'],
     },
     classes: false,
 });
