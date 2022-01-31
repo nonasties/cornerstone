9cd5c8ea414ef1d4af041da2a326f5d784d6bb15
Tony McCreath <tony@websiteadvantage.com.au>
2022-01-31 11:43:58 +1030

    more work on font loading

--- assets/js/theme/common/font.js
+++ assets/js/theme/common/font.js
@@ -1,14 +1,3 @@
 const WebFont = require('webfontloader');
-// doing all fonts cause a big delay in active. need to do only what is in the CSS
-// families: ['Karla:n4', 'Roboto:n4', 'Volkhov:n4', 'Montserrat:n4,n5', 'Open+Sans:n4,n7', 'Source Sans Pro:n4'],
-// ['Volkhov:n4', 'Montserrat:n4,n5', 'Source Sans Pro:n4']
-WebFont.load({
-    custom: {
-        families: window.webfontloader_families
-    },
-  //  loading: function () { console.log('loading'); },
-  //  active: function () { console.log('active'); },
- //   fontloading: function (familyName, fvd) { console.log('fontloading', familyName, fvd); },
- //   fontactive: function (familyName, fvd) { console.log('fontactive', familyName, fvd); },
-    classes: true
-});
+
+WebFont.load(window.WebFontConfig);
