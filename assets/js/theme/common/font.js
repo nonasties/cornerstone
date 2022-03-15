48c838bccfa2cf07a9f885db2f8713a8d40f4f1b
Tony McCreath <tony@websiteadvantage.com.au>
2022-03-15 11:09:48 +1030

    re-applied my modifications. Not tested

--- assets/js/theme/common/font.js
+++ assets/js/theme/common/font.js
@@ -1,13 +1,15 @@
 const WebFont = require('webfontloader');
 
-const linkEl = document.querySelector('link[href*="https://fonts.googleapis.com/css"]');
-const fontUrl = linkEl.getAttribute('href');
-const regexFontsCollection = /family=([^&]*)/gm;
-const families = regexFontsCollection.exec(fontUrl)[1].replace(/\+/gm, ' ').split('|');
+// I do it inline
+// const linkEl = document.querySelector('link[href*="https://fonts.googleapis.com/css"]');
+// const fontUrl = linkEl.getAttribute('href');
+// const regexFontsCollection = /family=([^&]*)/gm;
+// const families = regexFontsCollection.exec(fontUrl)[1].replace(/\+/gm, ' ').split('|');
 
-WebFont.load({
-    custom: {
-        families,
-    },
-    classes: false,
-});
+// WebFont.load({
+//     custom: {
+//         families,
+//         text: 'abcdefghijklmnopqrstuvwxyz!'
+//     },
+//     events: false
+// });
