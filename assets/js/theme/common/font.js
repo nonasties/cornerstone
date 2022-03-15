237c0c5002024f3a4f8ab6092d7f4ba4bbfd6fcd
Tony McCreath <tony@websiteadvantage.com.au>
2022-03-15 10:47:14 +1030

    Updated to Cornerstone 6.3.0

--- assets/js/theme/common/font.js
+++ assets/js/theme/common/font.js
@@ -1,3 +1,13 @@
 const WebFont = require('webfontloader');
 
-WebFont.load(window.WebFontConfig);
+const linkEl = document.querySelector('link[href*="https://fonts.googleapis.com/css"]');
+const fontUrl = linkEl.getAttribute('href');
+const regexFontsCollection = /family=([^&]*)/gm;
+const families = regexFontsCollection.exec(fontUrl)[1].replace(/\+/gm, ' ').split('|');
+
+WebFont.load({
+    custom: {
+        families,
+    },
+    classes: false,
+});
